6f6afde88b5be46107875be0340eb9bad6b616cf
thomasSDK <tsdkelly@protonmail.com>
2020-07-21 18:33:58 +0100

    upload artifact
    working

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const artifact = require("@actions/artifact");
 
 const BuildScheme = core.getInput("BuildScheme");
 const Project = core.getInput("project");
-
-const regex = /TARGET_BUILD_DIR = ([\/-\w]+)\n/;
-let buildDirectory = "";
-let myError = "";
-const options = {};
+const UploadArtifact = core.getInput("UploadArtifact");
 
 const BuildProject = `${Project}.xcodeproj`;
 
@@ -17,34 +13,8 @@ async function run() {
   try {
     if (BuildScheme === "PopCameraDevice_Osx") {
       await exec.exec("brew", ["install", "nuget"]);
-      options.cwd = "./Source/libs";
-      await exec.exec("nuget", ["install"], options);
+      await exec.exec("nuget", ["install"], { cwd: "./Source/libs" });
     }
-
-    // Get BuildDirectory
-    const outputOptions = {};
-    outputOptions.listeners = {
-      stdout: (data) => {
-        buildDirectory += data.toString();
-        console.log(regex.exec(buildDirectory));
-        buildDirectory = regex.exec(buildDirectory);
-      },
-      stderr: (data) => {
-        myError += data.toString();
-      },
-    };
-
-    await exec.exec(
-      "xcodebuild",
-      [
-        `-workspace`,
-        `${BuildProject}/project.xcworkspace`,
-        `-scheme`,
-        `${BuildScheme}`,
-        `-showBuildSettings`,
-      ],
-      outputOptions
-    );
 
     await exec.exec("xcodebuild", [
       `-workspace`,
@@ -61,9 +31,60 @@ async function run() {
       `Release`,
     ]);
 
-    console.log(buildDirectory)
+    if (UploadArtifact) {
+      // Get BuildDirectory
+      const regex = /TARGET_BUILD_DIR = ([\/-\w]+)\n/;
+      let buildDirectory = "";
+      let myError = "";
+      const outputOptions = {};
+      outputOptions.listeners = {
+        stdout: (data) => {
+          buildDirectory += data.toString();
+          console.log(regex.exec(buildDirectory));
+          buildDirectory = regex.exec(buildDirectory);
+        },
+        stderr: (data) => {
+          myError += data.toString();
+        },
+      };
 
-    core.setOutput('buildDirectory', (buildDirectory[0]));
+      await exec.exec(
+        "xcodebuild",
+        [
+          `-workspace`,
+          `${BuildProject}/project.xcworkspace`,
+          `-scheme`,
+          `${BuildScheme}`,
+          `-showBuildSettings`,
+        ],
+        outputOptions
+      );
+
+      buildDirectory = buildDirectory[1];
+      await exec.exec("ls", [buildDirectory]);
+
+      const artifactClient = artifact.create();
+      const artifactName = `${BuildScheme}.framework`;
+
+      await exec.exec(
+        "zip",
+        ["-r", `${BuildScheme}.framework.zip`, `${BuildScheme}.framework`],
+        { cwd: buildDirectory }
+      );
+
+      const files = [`${buildDirectory}/${BuildScheme}.framework.zip`];
+      const rootDirectory = buildDirectory;
+      const options = {
+        continueOnError: true,
+      };
+
+      const uploadResult = await artifactClient.uploadArtifact(
+        artifactName,
+        files,
+        rootDirectory,
+        options
+      );
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
