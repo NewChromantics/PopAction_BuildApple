0cd22e2ea2825bea0d1d28226d860951e1ddd238
Graham Reeves <graham@grahamreeves.com>
2020-08-09 21:39:49 +0100

    Regex syntax still wrong

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run()
         Line = Line.toString(); //  gr; is this not a string?
         //  extract all matches and add to our list
         const Lines = Line.split('\n');
-        let Matches = Lines.map( Line => regex1.exec(Line) );
+        let Matches = Lines.map( Line => Regex.exec(Line) );
         Matches = Matches.filter( Line => Line!=null );
         Matches = Matches.map( Line => Line[1] );
         BuildDirectorys.push( ...Matches );
